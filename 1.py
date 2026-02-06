c2fe9c17133bc83ade80f3fe82e516b38e0b0aa6
Evan Lee <evanlee@EvandeMacBook-Pro.local>
2026-02-06 10:05:56 +0800

    第二次提交

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-def calculate_circle_area(radius):
+'''def calculate_circle_area(radius):
     area = 3.14 * radius ** 2
     return area
     
@@ -7,3 +7,17 @@ radius = float(input("输入半径:"))
 result = calculate_circle_area(radius)
 
 print(result)
+'''
+
+def calculate_discount(price,rate):
+    discount = price * rate
+    return discount
+
+price = float(input("原价:"))
+rate = float(input("折扣率:"))
+
+result = calculate_discount(price,rate)
+
+print(result)
+
+
